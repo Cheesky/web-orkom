ae5fd88557c70043d01fbc4e97358d78d9ecd653
Cheesky <ruby.akbar.firmansyah@gmail.com>
2025-07-28 07:13:19 +0700

    Yah

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,20 @@
 <?php
+//railway DB
 $host = "interchange.proxy.rlwy.net";
 $user = "root";
 $password = "JMEVYjBfpTuvMkJftlnzRyQmrZkwKCxk";
 $dbname = "orkom";
 
+//Website InfinityFree Hostingan DB (Uncomment to change)
+//$host = "sql111.infinityfree.com";
+//$user = "if0_39482843";
+//$password = "7lvstKImTR";
+//$dbname = "if0_39482843_tungbes";
+
+
 // Optional: port (if not default 3306)
 $port = 59010;
+//$port = 3306; //InfinityFree Port
 
 // Create connection
 $conn = mysqli_connect($host, $user, $password, $dbname, $port);
